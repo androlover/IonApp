660743eb69cfc23bf3bf81156a2c8df565c9731c
androlover <roshan.kumar@yuvaap.com>
2025-09-02 17:16:48 +0530

    cahngers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB4sVXaNCChKP5tbObgM6qCsuSGVjwSCiI",
+    authDomain: "yuvaap-ionic.firebaseapp.com",
+    databaseURL: "https://yuvaap-ionic-default-rtdb.firebaseio.com",
+    projectId: "yuvaap-ionic",
+    storageBucket: "yuvaap-ionic.firebasestorage.app",
+    messagingSenderId: "334823379096",
+    appId: "1:334823379096:web:d71449c1b49ff7a89c00ed",
+    measurementId: "G-PC6CCPMVCP"
+  }
 };
